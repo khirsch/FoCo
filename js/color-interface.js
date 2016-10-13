dd6e465fe1502d90bfc2dad018dc0165c07bc4c8
Katrina Hirsch and Zachary Matthewstearn <zachary@>
2016-10-13 14:54:34 -0700

    add farbtastic jquery plugin

--- js/color-interface.js
+++ js/color-interface.js
@@ -2,19 +2,11 @@ var Color = require('./../js/color.js').colorModule;
 var CustomError = require('./../js/custom-error.js').customErrorModule;
 
 $(document).ready(function() {
-
-  $('#input').submit(function(event){
+  $('#colorpicker').click(function(event){
     event.preventDefault();
-    var hex = $('#input-color').val();
+    $('#colorpicker').farbtastic('#color');
+    var hex = $('#color').val();
     var color = new Color(hex);
     color.getColorInfo();
-
-
-    // var testError = new CustomError();
-    // testError.test();
-
   });
-
-
-
 });
